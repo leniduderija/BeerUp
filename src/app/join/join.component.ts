c4bc3e14b318d9b0144a6d61a15cc73c52289276
Leni Duderija <lduderija@snowfall.se>
2018-04-15 20:46:18 +0200

    join page changes

--- src/app/join/join.component.ts
+++ src/app/join/join.component.ts
@@ -1,10 +1,19 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
+import { Router } from '@angular/router';
+
+import { AlertService } from '../core/alert.service';
 
 import { User } from '../core/models/user.model';
 
+import { UserService } from '../core/user.service';
+
+// used to create fake backend
+import { fakeBackendProvider } from '../core/helpers/fake-backend';
+
 
 @Component({
+  moduleId: module.id,
   selector: 'app-join',
   templateUrl: './join.component.html',
   styleUrls: ['./join.component.scss']
@@ -14,6 +23,8 @@ export class JoinComponent implements OnInit {
   // model: User;
 
   user = new User('', '', '', '', '', '', false);
+  loading = false;
+  created = false;
 
   rsvpOptions = [
     {
@@ -30,12 +41,37 @@ export class JoinComponent implements OnInit {
     }
   ];
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private _userService: UserService,
+    private alertService: AlertService
+  ) { }
 
   ngOnInit() {
   }
 
+  
+
   onSubmitJoinForm() {
       console.log(this.user);
+
+      this.loading = true;
+
+      this._userService.createUser(this.user)
+      .subscribe(
+          data => {
+              console.debug('Success! ', data);
+              this.loading = false;
+              this.created = true;
+              // this.user = new User('', '', '', '', '', '', false);
+              this.alertService.success('Registration successful', true);
+              // this.router.navigate(['home']);
+          },
+          error => {
+            console.debug('Error! ', error);
+            this.alertService.error(error);
+            this.loading = false;
+          });
+
   }
 }
--- src/app/join/join.module.ts
+++ src/app/join/join.module.ts
@@ -6,12 +6,23 @@ import { FormsModule } from "@angular/forms";
 import { JoinComponent } from './join.component';
 import { JoinRoutingModule } from './join-routing.module';
 
+import { AlertComponent } from '../core/directives/alert/alert.component';
+
+import { UserService } from '../core/user.service';
+
+// used to create fake backend
+import { fakeBackendProvider } from '../core/helpers/fake-backend';
+
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     JoinRoutingModule
   ],
-  declarations: [JoinComponent]
+  declarations: [JoinComponent, AlertComponent],
+  providers: [
+    // provider used to create fake backend
+    fakeBackendProvider
+  ]
 })
 export class JoinModule { }
