8fbf6f778041bbd81b65dd0fb128beb553bb828a
Leni Duderija <lduderija@snowfall.se>
2018-04-20 22:20:10 +0200

    route change

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -2,15 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home.component';
 import { MainComponent } from '../layout/main/main.component';
-// import { SidebarComponent } from './sidebar/sidebar.component';
 
 
 const routes: Routes = [{
   path: '',
   component: MainComponent,
   children: [
-    { path: 'home', component: HomeComponent},
-    // { path: '', component: SidebarComponent, outlet: 'sidebar'}
+    { path: 'home', component: HomeComponent}
   ]
 }];
 
